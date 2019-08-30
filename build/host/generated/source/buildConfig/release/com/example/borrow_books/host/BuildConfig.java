ac893153efad90e676ca14aebd2ee4200d81d5fa
Qson8 <455063392@qq.com>
2019-08-30 11:52:29 +0800

    提交README

---
+++ build/host/generated/source/buildConfig/release/com/example/borrow_books/host/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.borrow_books.host;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.qson.borrow_books.host";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
